618917901abc6bf0ca98bd05e9ca52499e747429
Gokhan Egri <egrigokhan@gmail.com>
2021-08-27 00:17:07 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,13 +9,13 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
       url: "https://rpc-mumbai.matic.today",
       accounts: [privateKey]
     },
+    /*
     matic: {
       // Infura
       // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
